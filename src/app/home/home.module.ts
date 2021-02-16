c3101377d90881b70aadbefb5ef9d0c85274105a
Gabriel Carneiro <carneiro.development@gmail.com>
2021-02-16 13:56:09 -0400

    fix: Sidenav in mobile viewport

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,11 +7,24 @@ import { HomeComponent } from './home.component';
 
 import { SharedModule } from './../shared/shared.module';
 
-import { DashboardModule } from './../core/dashboard/dashboard.module'
+import { DashboardModule } from './../core/dashboard/dashboard.module';
 import { ShoppingCartModule } from '../shopping-cart/shopping-cart.module';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, RouterModule, SharedModule, DashboardModule, ShoppingCartModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    RouterModule,
+    SharedModule,
+    DashboardModule,
+    ShoppingCartModule,
+    MatIconModule,
+    HttpClientModule,
+    OverlayModule,
+  ],
 })
 export class HomeModule {}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   DataStoredInToken,
   SessionService,
 } from 'src/app/common/services/session.service';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
@@ -12,8 +13,7 @@ import {
 })
 export class HeaderComponent implements OnInit {
   session: DataStoredInToken;
-
-  constructor(private sessionService: SessionService, private router: Router) {}
+  showFiller = false;
 
   ngOnInit(): void {
     this.session = this.sessionService.decodeSession();
@@ -23,4 +23,29 @@ export class HeaderComponent implements OnInit {
     this.sessionService.logOut();
     this.router.navigate(['auth']);
   }
+
+  mobileQuery: MediaQueryList;
+
+  // fillerNav = Array.from({ length: 3 }, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private sessionService: SessionService,
+    private router: Router
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  // shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some((h) =>
+  //   h.test(window.location.host)
+  // );
 }
